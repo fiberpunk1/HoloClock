a73cfb53da0502f6cf85e9ce1cf17c4fbc469ecf
fiber-punk <contact@fiber-punk.com>
2023-06-07 10:31:59 +0800

    :apple: version 1.0

---
+++ Firmware/Holo-fw/src/app/watcher/watcher.h
@@ -0,0 +1,11 @@
+#ifndef _HOLO_WATCHER_H
+#define _HOLO_WATCHER_H
+
+#define EN_TIME_ZONE
+// #define ZH_TIME_ZONE
+
+extern void watch_init();
+extern void set_focus_update(int v);
+extern void watch_release();
+extern void update_time_status();
+#endif
